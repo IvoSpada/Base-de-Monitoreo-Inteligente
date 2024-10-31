05c446dd615ff706df420824d183cbfeee3bfad2
marc0jpg <marcotaliente123@gmail.com>
2024-10-31 16:07:13 -0300

    Error en JS

--- assets/php/footprint.php
+++ assets/php/footprint.php
@@ -25,8 +25,39 @@ if ($result->num_rows > 0) {
     }
 }
 
-echo json_encode($data);
+json_encode(['country' => $data]);
+header("Location: http://bim/calculadora.html");
+
 
 $conn->close();
+
+/*
+$y = 3; // Example value for y
+$x = 0; // Initialize x
+
+switch (true) {
+    case ($y >= 0 && $y <= 5):
+        $x = 10; // Set x to 10 if y is between 0 and 5
+        break;
+    // Add more cases as needed for other ranges
+    default:
+        $x = 0; // Default value for x
+}
+
+echo json_encode(['x' => $x]); // Return the value of x as JSON
+
+javascript:
+
+fetch('get_x.php')
+    .then(response => response.json())
+    .then(data => {
+        var x = data.x; // Get the value of x from the response
+        console.log("Value of x:", x); // Use x in your JavaScript
+    })
+    .catch(error => console.error('Error fetching x:', error));
+*/
 ?>
+
+
+
 
